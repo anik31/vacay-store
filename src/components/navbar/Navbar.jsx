b598c9e81c5ce9bfe9e6f073a4707cc7077f3500
Aniket Prakash <b218004@iiit-bh.ac.in>
2022-04-05 19:15:34 +0530

    style: make navbar responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,26 +2,46 @@ import { Link } from "react-router-dom";
 import { useCart, useWishlist } from "../../context";
 import { logo } from "../../assets";
 import "./navbar.css";
+import {useState} from "react";
 
 export function Navbar(){
     const {cartState} = useCart();
     const {wishlistState} = useWishlist();
+    const [isHamburgerMenuVisible, setIsHamburgerMenuVisible] = useState(false);
+    const hamburgerMenuData = [
+        { linkTo: "/", linkFor: "Home", icon: "fas fa-home" },
+        { linkTo: "/products", linkFor: "Products", icon: "fas fa-store" },
+        { linkTo: "/wishlist", linkFor: "Wishlist", icon: "fas fa-heart" },
+        { linkTo: "/cart", linkFor: "Cart", icon: "fas fa-shopping-cart" },
+        { linkTo: "/login", linkFor: "Login/Sign Up", icon: "fas fa-user" },
+    ]
 
     return (
         <header className="header">
         <div className="hamburger-logo-wrapper">
-            <button><i className="fas fa-bars"></i></button>
+            <button onClick={()=>setIsHamburgerMenuVisible(true)}><i className="fas fa-bars"></i></button>
             <Link to="/"><img src={logo} alt="vacay-store-logo" className="logo" /></Link>
         </div>
-        <nav className="hamburger-menu">
+        <nav className="ham-menu box-shadow" style={{display:isHamburgerMenuVisible?"block":"none"}}>
             <ul>
-                <li><button><i className="fas fa-times"></i></button></li>
-                <li><Link to="/login" className="btn btn-secondary-link">Login</Link></li>
-                <li><Link to="/wishlist" className="btn btn-secondary-link">Wishlist</Link></li>
-                <li><Link to="/cart" className="btn btn-secondary-link">Cart</Link></li>
+                <li>
+                    <Link to="/login" className="ham-item">
+                        <i className="fas fa-user"></i>
+                    Login/ Sign Up</Link>
+                    <button onClick={()=>setIsHamburgerMenuVisible(false)}>
+                        <i className="fas fa-times"></i>
+                    </button>
+                </li>
+                {hamburgerMenuData.map(item=>
+                <li key={item.linkTo}>
+                    <Link to={item.linkTo} className="ham-item">
+                        <i className={item.icon}></i>
+                        <span>{item.linkFor}</span>
+                    </Link>
+                </li>)}
             </ul>
         </nav>
-        <nav className="navigation">
+        <nav className="navigation nav-hide">
             <ul>
                 <li><Link to="/" className="btn btn-secondary-link">Home</Link></li>
                 <li><Link to="/products" className="btn btn-secondary-link">Products</Link></li>
@@ -31,12 +51,17 @@ export function Navbar(){
             <i className="fas fa-search"></i>
             <input type="text" placeholder="Search" />
         </div>
-        <nav className="navigation nav-hide">
+        <nav className="navigation">
             <ul>
-                <li><Link to="/login" className="btn btn-primary">Login</Link></li>
-                <li className="badge-wrapper">
-                    <Link to="/wishlist"><i className="btn-icon far fa-heart"></i></Link>
-                    {wishlistState.length!==0 && <span className="badge badge-number">{wishlistState.length}</span>}
+                <li className="nav-hide"><Link to="/login" className="btn btn-primary">Login</Link></li>
+                <li>
+                    <Link to="/wishlist" className="btn btn-secondary-icon-text-no-border">
+                        <div className="badge-wrapper">
+                            <i className="btn-icon fas fa-heart"></i>
+                            {wishlistState.length!==0 && <span className="badge badge-number">{wishlistState.length}</span>}
+                        </div>
+                        <span className="nav-hide">Wishlist</span>
+                    </Link>
                 </li>
                 <li>
                     <Link to="/cart" className="btn btn-secondary-icon-text-no-border">
@@ -44,7 +69,7 @@ export function Navbar(){
                             <i className="btn-icon fas fa-shopping-cart"></i>
                             {cartState.length!==0 && <span className="badge badge-number">{cartState.length}</span>}
                         </div>
-                        Cart
+                        <span className="nav-hide">Cart</span>
                     </Link>
                 </li>
             </ul>
