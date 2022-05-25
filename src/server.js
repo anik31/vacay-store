4b819398dd1808ee97afc15d520c0be0d92523e7
Aniket Prakash <b218004@iiit-bh.ac.in>
2022-05-25 22:02:42 +0530

    add address & coupon controllers to backend

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 import { Server, Model, RestSerializer } from "miragejs";
 import {
+  addNewAddressHandler,
+  getAllAddressesHandler,
+  removeAddressHandler,
+} from "./backend/controllers/AddressController";
+import {
   loginHandler,
   signupHandler,
   verifyUser
@@ -14,6 +19,7 @@ import {
   getAllCategoriesHandler,
   getCategoryHandler,
 } from "./backend/controllers/CategoryController";
+import { getAllCouponsHandler } from "./backend/controllers/CouponController";
 import {
   getAllProductsHandler,
   getProductHandler,
@@ -39,6 +45,7 @@ export function makeServer({ environment = "development" } = {}) {
       user: Model,
       cart: Model,
       wishlist: Model,
+      address: Model
     },
 
     // Runs on the start of the server
@@ -71,6 +78,9 @@ export function makeServer({ environment = "development" } = {}) {
       this.get("/categories", getAllCategoriesHandler.bind(this));
       this.get("/categories/:categoryId", getCategoryHandler.bind(this));
 
+      // coupons route (public)
+      this.get("/coupon", getAllCouponsHandler.bind(this));
+
       // cart routes (private)
       this.get("/user/cart", getCartItemsHandler.bind(this));
       this.post("/user/cart", addItemToCartHandler.bind(this));
@@ -87,6 +97,11 @@ export function makeServer({ environment = "development" } = {}) {
         "/user/wishlist/:productId",
         removeItemFromWishlistHandler.bind(this)
       );
+
+      // address routes (private)
+      this.get("/user/addresses", getAllAddressesHandler.bind(this));
+      this.post("/user/address", addNewAddressHandler.bind(this));
+      this.delete("/user/address/:addressId", removeAddressHandler.bind(this));
     },
   });
 }
